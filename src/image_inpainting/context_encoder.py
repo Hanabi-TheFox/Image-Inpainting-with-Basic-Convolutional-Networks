81d678990f0e98496c7e9073d28e536f8591818b
RobinMeneust <robin.meneust@hotmail.fr>
2024-12-11 00:32:20 +0100

    Fixed losses + Added val and test

--- src/image_inpainting/context_encoder.py
+++ src/image_inpainting/context_encoder.py
@@ -7,7 +7,7 @@ from image_inpainting.adversarial_discriminator import AdversarialDiscriminator
 from image_inpainting.adversarial_generator import AdversarialGenerator
 from image_inpainting.encoder import Encoder
 from image_inpainting.decoder import Decoder
-from image_inpainting.loss import JointLoss
+from image_inpainting.loss import JointLoss, ReconstructionLoss, AdversarialLoss
 from image_inpainting.tiny_image_net_data_module import TinyImageNetDataModule
 
 # Based on https://lightning.ai/docs/pytorch/stable/notebooks/lightning_examples/basic-gan.html
@@ -15,11 +15,14 @@ class ContextEncoder(pl.LightningModule):
     def __init__(self, input_size=(3, 128, 128), hidden_size=4000, reconstruction_loss_weight = 0.999, adversarial_loss_weight = 0.001):
         super(ContextEncoder, self).__init__()
 
+        self.automatic_optimization = False # https://lightning.ai/docs/pytorch/stable/model/manual_optimization.html
+
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.psnr_metric = torchmetrics.image.PeakSignalNoiseRatio()
-        self.loss_function = JointLoss(reconstruction_loss_weight=reconstruction_loss_weight, adversarial_loss_weight=adversarial_loss_weight)
-
+        self.joint_loss = JointLoss(reconstruction_loss_weight=reconstruction_loss_weight, adversarial_loss_weight=adversarial_loss_weight)
+        self.rec_loss = ReconstructionLoss()
+        self.adv_loss = AdversarialLoss()
         self.test_psnr = 0
 
         self.generator = AdversarialGenerator(input_size=input_size, hidden_size=hidden_size)
@@ -31,21 +34,29 @@ class ContextEncoder(pl.LightningModule):
 
     def configure_optimizers(self):
         # They use a higher learning rate for context encoder (10 times) to that of adversarial discriminator (page 6 of the paper)
-        generator_optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        generator_optimizer = torch.optim.Adam(self.generator.parameters(), lr=1e-3)
         discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=1e-4)
 
         return [generator_optimizer, discriminator_optimizer], [] # [] because there is no scheduler
 
     def training_step(self, batch, batch_idx):
-        img, unmasked_image = batch
-        outputs = self.forward(x)
-        loss_val = self.loss_function(outputs, y) # joint loss
-        psnr_val = self.psnr_metric(outputs, y)
+        masked_imgs, true_masked_regions = batch
 
-        generator_optimizer, discriminator_optimizer = self.get_optimizers()
+        generator_outputs = self.forward(masked_imgs)
+
+        psnr_val = self.psnr_metric(generator_outputs, true_masked_regions)
+
+        generator_optimizer, discriminator_optimizer = self.optimizers()
 
         # Generator
         self.toggle_optimizer(generator_optimizer) # we don't want to update the discriminator here
+        loss_val = self.joint_loss(
+            context_encoder_outputs=generator_outputs,
+            true_masked_regions=true_masked_regions,
+            discriminator_fake_predictions=self.discriminator(generator_outputs),
+            discriminator_real_predictions=self.discriminator(true_masked_regions)
+        )
+
         self.manual_backward(loss_val)
         generator_optimizer.step()
         generator_optimizer.zero_grad()
@@ -53,39 +64,50 @@ class ContextEncoder(pl.LightningModule):
 
         # Discriminator
         self.toggle_optimizer(discriminator_optimizer)  # we don't want to update the generator here
+
+        # these three steps were not really well explained in the paper, but it's a common practice to do it
+        # and this is also described here: https://lightning.ai/docs/pytorch/stable/notebooks/lightning_examples/basic-gan.html
+        loss_val = self.adv_loss(fake_predictions=self.discriminator(generator_outputs.detach()), real_predictions=self.discriminator(true_masked_regions))
         self.manual_backward(loss_val)
         discriminator_optimizer.step()
         discriminator_optimizer.zero_grad()
         self.untoggle_optimizer(discriminator_optimizer)
 
-        self.log('train_acc', psnr_val, prog_bar=True)
+        self.log('train_psnr', psnr_val, prog_bar=True)
         self.log('train_loss', loss_val, prog_bar=True)
         return loss_val
 
     def validation_step(self, batch, batch_idx):
-        pass
-        # x,y = batch
-        # loss_function = nn.CrossEntropyLoss()
-        #
-        # outputs = self.forward(x)
-        # loss = loss_function(outputs, y)
-        # psnr = self.psnr_metric(outputs, y)
-        #
-        # self.log('val_acc', psnr, prog_bar=True)
-        # self.log('val_loss', loss, prog_bar=True)
+        masked_imgs, true_masked_regions = batch
+        generator_outputs = self.forward(masked_imgs)
+
+        psnr_val = self.psnr_metric(generator_outputs, true_masked_regions)
+        loss_val = self.joint_loss(
+            context_encoder_outputs=generator_outputs,
+            true_masked_regions=true_masked_regions,
+            discriminator_fake_predictions=self.discriminator(generator_outputs),
+            discriminator_real_predictions=self.discriminator(true_masked_regions)
+        )
+
+        self.log('val_psnr', psnr_val, prog_bar=True)
+        self.log('val_loss', loss_val, prog_bar=True)
 
     def test_step(self, batch, batch_idx):
-        pass
-        # x,y = batch
-        # loss_function = nn.CrossEntropyLoss()
-        #
-        # outputs = self.forward(x)
-        # loss = loss_function(outputs, y)
-        #
-        # self.test_psnr += self.psnr_metric(outputs, y)
-        #
-        # self.log('test_loss', loss, prog_bar=True)
-        # self.log('test_psnr', self.test_psnr / (batch_idx+1), prog_bar=True)
+        masked_imgs, true_masked_regions = batch
+        generator_outputs = self.forward(masked_imgs)
+
+        psnr_val = self.psnr_metric(generator_outputs, true_masked_regions)
+        loss_val = self.joint_loss(
+            context_encoder_outputs=generator_outputs,
+            true_masked_regions=true_masked_regions,
+            discriminator_fake_predictions=self.discriminator(generator_outputs),
+            discriminator_real_predictions=self.discriminator(true_masked_regions)
+        )
+
+        self.test_psnr += psnr_val
+
+        self.log('test_psnr', psnr_val, prog_bar=True)
+        self.log('test_loss', loss_val, prog_bar=True)
 
     def on_test_epoch_start(self):
         self.test_psnr = 0
--- src/image_inpainting/loss.py
+++ src/image_inpainting/loss.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn, log
 from torch.linalg import matrix_norm
 
@@ -9,33 +10,41 @@ class JointLoss(nn.Module):
         self.rec_loss = ReconstructionLoss()
         self.adv_loss = AdversarialLoss()
 
-    def forward(self, predictions, targets):
-        rec_loss_val = self.rec_loss(predictions, targets)
-        adv_loss_val = self.adv_loss(predictions, targets)
-        return self.rec_loss_weight * rec_loss_val + self.classification_loss_weight * adv_loss_val
+    def forward(self, context_encoder_outputs, true_masked_regions, discriminator_fake_predictions, discriminator_real_predictions):
+        rec_loss_val = self.rec_loss(context_encoder_outputs, true_masked_regions) # generator loss
+        adv_loss_val = self.adv_loss(discriminator_fake_predictions, discriminator_real_predictions) # discriminator loss
+
+        return self.rec_loss_weight * rec_loss_val + self.adv_loss_weight * adv_loss_val
 
 class ReconstructionLoss(nn.Module):
     def __init__(self):
         super(ReconstructionLoss, self).__init__()
 
-    def forward(self, encoder_out_masked_x, masks, inputs):
-        # the element wise product in the paper is the Hadamard product
-        # F is the encoder model
+    def forward(self, context_encoder_outputs, true_masked_regions):
+        # compared to the paper:
+        # - context_encoder_outputs is mask * F((1-M) * x): the generated inner part of the image
+        # - true_masked_regions is mask * true_masked_regions is masked : the real inner part of the image
 
-        # encoder_out_masked_x is F(Hadamard_prod((1-mask), input))
-        # val = Hadamard product of mask and prediction
-        val = masks * encoder_out_masked_x # TODO not sure if this is correct, check again
         # normalized distance L2
-        return matrix_norm(val - inputs, ord=2) # TODO or MSE ?
+        diff = context_encoder_outputs - true_masked_regions
+        l2_cubed = torch.norm(diff, p=2, dim=(1, 2, 3)) ** 2
+        return l2_cubed.mean() # mean over the batch
+
 
 class AdversarialLoss(nn.Module):
     def __init__(self):
         super(AdversarialLoss, self).__init__()
 
-    def forward(self, decoder_out_dx, decoder_out_masked_dfx):
-        # decoder_out_dx is the output of the decoder model for x (D(x))
-        # decoder_out_masked_dfx is the output of the decoder model for the output of the encoder (D(F(Hadamard_prod((1-mask), input))))
-        log_out_x = log(decoder_out_dx)
-        log_out_masked_x = log(1 - decoder_out_masked_dfx)
+    def forward(self, fake_predictions, real_predictions):
+        # we add squeeze(-1) to remove the last dimension (e.g. (32, 1) -> (32))
 
-        return log_out_x + log_out_masked_x # TODO check if this is correct, this is not exactly the same as how it's in the paper
+        # how bad it is at labelling the true image as real (log(1) = 0 is what we want, log(0) -> -inf is bad)
+        log_d_real = log(real_predictions).squeeze(-1)
+
+        # how bad is it at labelling the generated image as fake (same, and here we invert the label of fake_predictions)
+        log_d_fake = log(1 - fake_predictions).squeeze(-1)
+
+        # the "E" in the formula in the paper seems to be the mean (E(X))
+        # Here the model is trained to maximize the logistic likelihood, but we need to return a loss to be minimized, so we just take the opposite of the given formula
+        loss_val = -((log_d_real + log_d_fake) / 2)
+        return loss_val.mean() # mean over the batch
