Fixed rec loss (the factor was inverted) + fixed normalization (was not aligned with paper) + changed loss to be aligned with cited paper in original paper

src/image_inpainting/datamodule/image_net_data_module.py
@@ -14,22 +14,15 @@ from image_inpainting.utils import insert_image_center
 # AIIP Exercises & https://lightning.ai/docs/pytorch/stable/data/datamodule.html
 
 class ImageNetDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir="../../data/imagenet", batch_size_train=32, batch_size_val=32, batch_size_test=32, num_workers=0, pin_memory=False, persistent_workers=False, normalize_to_0_1=False):
+    def __init__(self, data_dir="../../data/imagenet", batch_size_train=32, batch_size_val=32, batch_size_test=32, num_workers=0, pin_memory=False, persistent_workers=False):
         super().__init__()
         self.data_dir = data_dir
-        self.normalize_to_0_1 = normalize_to_0_1
         
-        transforms_operations = [
+        self.transform = transforms.Compose([
             transforms.Resize((128, 128)), # Resize image to 128x128
-            transforms.ToTensor() # Convert image to tensor (normalized to [0,1])
-        ]
-
-        if normalize_to_0_1:
-            self.transform = transforms.Compose(transforms_operations)
-        else:
-            self.transform = transforms.Compose(transforms_operations + 
-                [transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]  # Normalize for ImageNet
-            )
+            transforms.ToTensor(), # Convert image to tensor (normalized to [0,1])
+            transforms.Normalize(mean=0.5, std=0.5) # Normalize to -1 1
+        ])
             
         self.batch_size_train, self.batch_size_val, self.batch_size_test = batch_size_train, batch_size_val, batch_size_test
         self.train = None
@@ -39,14 +32,8 @@ class ImageNetDataModule(pl.LightningDataModule):
         self.pin_memory = pin_memory
         self.persistent_workers = persistent_workers
 
-    def inverse_transform(self, x):
-        if self.normalize_to_0_1:
-            inverse_transformed_x = x # already on 0 1 scale
-        else:
-            mean = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-            std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)      
-            inverse_transformed_x = x * std + mean
-            
+    def inverse_transform(self, x):    
+        inverse_transformed_x = x * 0.5 + 0.5 # denormalize to [0, 1]
         inverse_transformed_x = inverse_transformed_x.permute(1, 2, 0).numpy() # (C, H, W) -> (H, W, C) 
         inverse_transformed_x = (inverse_transformed_x * 255).astype(int)
         

src/image_inpainting/loss.py
@@ -82,6 +82,8 @@ class ReconstructionLoss(nn.Module):
         # - true_masked_regions is mask * true_masked_regions is masked : the real inner part of the image
 
         # normalized distance L2
+        
+        # Here overlap is the region in the 7px border, and non-overlap is region inside
 
         mask_overlap = torch.ones_like(true_masked_regions)
         # dim 1 is batch, dim 2 is channels, dim 3 is height, dim 4 is width
@@ -92,16 +94,13 @@ class ReconstructionLoss(nn.Module):
         diff = context_encoder_outputs - true_masked_regions
         overlap_diff = diff * mask_overlap
         non_overlap_diff = diff * mask_non_overlap
-        
-        # l2_cubed_overlap_diff = (torch.norm(overlap_diff, p=2, dim=(1, 2, 3)) ** 2) # not on the batch dimension
-        # l2_cubed_non_overlap_diff = (torch.norm(non_overlap_diff, p=2, dim=(1, 2, 3)) ** 2) # not on the batch dimension
-        
+                
         # We use mse instead of l2, as they do in the lua version of the paper
-        l2_cubed_overlap_mse = (overlap_diff ** 2).mean(dim=(1, 2, 3))
-        l2_cubed_non_overlap_mse = (non_overlap_diff ** 2).mean(dim=(1, 2, 3))
+        l2_cubed_overlap_mse = (overlap_diff ** 2).mean(dim=(1, 2, 3)) # not on the batch dimension
+        l2_cubed_non_overlap_mse = (non_overlap_diff ** 2).mean(dim=(1, 2, 3)) # not on the batch dimension
         
         # Mean over batch
-        loss = l2_cubed_overlap_mse + self.overlapping_weight_factor * l2_cubed_non_overlap_mse
+        loss = l2_cubed_overlap_mse * self.overlapping_weight_factor + l2_cubed_non_overlap_mse
         
         return loss.mean() # mean over batch
 

src/image_inpainting/model/context_encoder.py
@@ -25,10 +25,10 @@ class ContextEncoder(pl.LightningModule):
         generator (AdversarialGenerator): The generator model.
         discriminator (AdversarialDiscriminator): The discriminator model.
         save_image_per_epoch (bool): Whether to save the first inpainted image (from the validation set) per epoch or not. Default is False.
-        lr_g (float): The learning rate for the generator. Default is 1e-3. Because the paper says "We use a higher learning rate for context encoder (10 times) to that of adversarial discriminator"
-        lr_d (float): The learning rate for the discriminator. Default is 1e-4.
+        lr_g (float): The learning rate for the generator. Default is 0.002 as mentioned in the cited paper. Because the paper says "We use a higher learning rate for context encoder (10 times) to that of adversarial discriminator"
+        lr_d (float): The learning rate for the discriminator. Default is 0.0002.
     """
-    def __init__(self, input_size=(3, 128, 128), hidden_size=4000, reconstruction_loss_weight = 0.999, adversarial_loss_weight = 0.001, save_image_per_epoch=False, lr_g=1e-3, lr_d=1e-4):
+    def __init__(self, input_size=(3, 128, 128), hidden_size=4000, reconstruction_loss_weight = 0.999, adversarial_loss_weight = 0.001, save_image_per_epoch=False, lr_g=0.002, lr_d=0.0002):
         """Initializes the ContextEncoder class.
         
         Args:
@@ -86,8 +86,8 @@ class ContextEncoder(pl.LightningModule):
             list: The list of schedulers (empty list in this case).
         """
         # They use a higher learning rate for context encoder (10 times) to that of adversarial discriminator (page 6 of the paper)
-        generator_optimizer = torch.optim.Adam(self.generator.parameters(), lr=self.lr_g, betas=(0.5, 0.999)) # suggested in the LUA code of the paper
-        discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=self.lr_d, betas=(0.5, 0.999)) # 1e-4 in the paper is for overlapping region (please refer to the losses in this package)
+        generator_optimizer = torch.optim.Adam(self.generator.parameters(), lr=self.lr_g, betas=(0.5, 0.9)) # suggested in the cited paper https://arxiv.org/pdf/1511.06434
+        discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=self.lr_d, betas=(0.5, 0.9)) # same as above
 
         return [generator_optimizer, discriminator_optimizer], [] # [] because there is no scheduler
 